b8d074300ea0a225ffb0675647476c398dae27e3
Daniel Ivanov <daniel@limechain.tech>
2019-03-01 15:25:32 +0200

    fix the Environment typo

--- constants/environments.js
+++ constants/environments.js
@@ -2,12 +2,10 @@ const BASE_SANDBOX = 'http://sandbox-api.limepay.io';
 const BASE_PRODUCTION = 'https://api.limepay.io';
 
 module.exports = {
-    Sandbox: {
-        Default: `${BASE_SANDBOX}/v1`,
-        v1: `${BASE_SANDBOX}/v1`
+    v1: {
+        Sandbox: `${BASE_SANDBOX}/v1`,
+        Production: `${BASE_PRODUCTION}/v1`
     },
-    Production: {
-        Default: `${BASE_PRODUCTION}/v1`,
-        v1: `${BASE_PRODUCTION}/v1`
-    }
+    Sandbox: `${BASE_SANDBOX}/v1`,
+    Production: `${BASE_PRODUCTION}/v1`
 }
